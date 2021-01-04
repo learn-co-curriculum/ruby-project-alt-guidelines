bc909947d4e58992113a79186595948c767ed20c
LaurenGifford <lauren.gifford.17@gmail.com>
2021-01-04 15:01:28 -0500

    adds methods to Pantry

--- app/pantry_app.rb
+++ app/pantry_app.rb
@@ -1,3 +1,24 @@
 class PantryApp
+    attr_accessor :user
+    include CliControls
+
+    def run
+        greet
+        login_or_signup
+        see_resources
+        goodbye
+    end
+
+    def greet
+    end
+
+    def login_or_signup
+    end
+
+    def see_resources
+    end
+
+    def goodbye
+    end
 
 end
---
+++ app/tools/cli_controls.rb
@@ -0,0 +1,4 @@
+module CliControls
+
+    @@prompt = TTY::Prompt.new
+end
