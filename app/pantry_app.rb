d5646c809779c5542eab74bc70546d835d542a41
LaurenGifford <lauren.gifford.17@gmail.com>
2021-01-05 10:20:04 -0500

    refactors delete method

--- app/pantry_app.rb
+++ app/pantry_app.rb
@@ -24,8 +24,7 @@ class PantryApp
         neighborhood = prompt.ask("What neighborhood do you live in?")
         self.user = User.find_or_create_by(username: username, neighborhood: neighborhood)
         puts "Thanks for signing in, #{user.username}!"
-        sleep(1.7)
- #       main_screen
+        sleep(1.75)
     end
 
     def main_screen
@@ -34,7 +33,7 @@ class PantryApp
         prompt.select("Do you want to manage your resources?") do |menu|
             menu.choice "Add a resource to my resources", -> {add_helper}
             menu.choice "View all of my resources", -> {view_helper}
-            menu.choice "Update a resource", -> {update_helper}
+            menu.choice "Update a resource nickname", -> {update_helper}
             menu.choice "Delete a resource", -> {delete_helper}
             menu.choice "Exit", -> {exit}
           end
@@ -45,7 +44,7 @@ class PantryApp
         selected_resource_id = prompt.select("Which resource do you wish to add?", resources)
         nickname = prompt.ask("Enter a nickname for this resource?")
         new_fav = FavResource.create(user_id: user.id, resource_id: selected_resource_id, nickname: nickname)
-        puts "Congratulations! You added #{new_fav.resource.name} to your resources."
+        puts "Congratulations! You added #{new_fav.resource.name} to your fav resources."
         sleep(2.0)
         main_screen
     end
@@ -67,7 +66,6 @@ class PantryApp
     end # return an instance of fav_resources
 
     def update_helper
-        #binding.pry
         select_helper
         nickname = prompt.ask("Enter a new nickname?")
         @selected_resource.update(nickname: nickname)
@@ -78,18 +76,13 @@ class PantryApp
 
     def delete_helper
         select_helper
-        answer = prompt.yes?("Delete this resource?")
-        if answer
-        @selected_resource.destroy
-        end
-        puts "You deleted #{selected_resource.nickname}."
+        answer = prompt.yes?("Delete this resource?", convert: :boolean)
+        answer ? @selected_resource.destroy : nil
+        puts "#{selected_resource.nickname} has been deleted from your fav resources."
         sleep(1.5)
         main_screen
         # my_resources = user.my_resources
         # selected_resource = prompt.select("Which resource would you like to delete?", my_resources.name)
-        # puts "#{deleted_resource.resource.name} has been deleted from your fav resources."
-        # sleep(2.0)
-        # main_screen
         # a list of all of the resources
         #select a resource
         #when the user clicks enter, that resource gets deleted from fav_resource
